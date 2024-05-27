068d0673073998cb4c7de0d96ecba1df92264961
Zarahi93 <adahi.g.z@hotmail.com>
2024-05-26 21:26:51 -0600

    b

--- test/numbers-validator/isNumberEven.spec.js
+++ test/numbers-validator/isNumberEven.spec.js
@@ -38,4 +38,34 @@ describe('isNumberEven', function () {
       validator.isNumberEven('5');
     }).to.throw('[5] is not of type "Number" it is of type "string"')
   })
+
+  
+  it('should return an array of even numbers', () => {
+    const arrayOfNumbers = [1,2,3,4,5,6];
+    const evenNumbersFromArray= validator.getEvenNumbersFromArray(arrayOfNumbers);
+    expect(evenNumbersFromArray).to.be.eql([2,4,6]);
+  });
+
+  it('should throw an error if all the elements of the array are not numbers', ()=>{
+  const arrayOfElements = [1,2,'3',4,5,6];
+  expect(()=>{
+    validator.getEvenNumbersFromArray(arrayOfElements);
+  }).to.throw('[1,2,3,4,5,6] is not an array of "Numbers"')
+  });
+
+  it('should throw an error if not an array', ()=>{
+const element = 4;
+expect(()=>{
+  validator.isAllNumbers(element);
+}).to.throw('[4] is not an array')
 });
+
+it('should return true if all are numbers', function () {
+ 
+  expect(validator.isAllNumbers([2,4,6])).to.be.equal(true);
+});
+
+
+
+});
+
